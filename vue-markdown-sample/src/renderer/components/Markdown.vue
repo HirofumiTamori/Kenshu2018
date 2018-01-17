6894989f42431df933cbfb2d527a3d51ec0fd678
Hiforumi Tamori <Hirofumi.Tamori@outlook.com>
2018-01-18 01:18:24 +0900

    File open / save dialog added

--- vue-markdown-sample/src/renderer/components/Markdown.vue
+++ vue-markdown-sample/src/renderer/components/Markdown.vue
@@ -4,8 +4,12 @@
       <textarea v-model="text" placeholder="入力してください"></textarea>
     </div>
     <div id="read-button">
-      <button @click="loadText">ロード</button>
+      <button @click="loadText">読み込み</button>
     </div>
+    <div id="write-button">
+      <button @click="saveText">書き出し</button>
+    </div>
+
     <div id="markdown-area">
       <vue-markdown :source="text"></vue-markdown>
     </div>
@@ -15,6 +19,7 @@
 <script>
 import VueMarkdown from 'vue-markdown'
 const fs = require('fs')
+const {dialog} = require('electron').remote
 
 export default {
   name: 'vuemarkdown',
@@ -29,14 +34,28 @@ export default {
   methods: {
     loadText () {
       console.log('read')
-      fs.readFile('initial.md', 'utf8', (err, data) => {
-        if (err) {
-          console.log(err)
-          return err
-        } else {
-          console.log('reading...')
-          this.text = data
-        }
+      dialog.showOpenDialog({
+        properties: ['openFile'],
+        filters: [{name: 'MarkDown', extensions: ['md']}]},
+      (fAry) => {
+        console.log(fAry)
+        fs.readFile(fAry[0], 'utf8', (err, data) => {
+          if (err) {
+            console.log(err)
+            return err
+          } else {
+            this.text = data
+          }
+        })
+      })
+    },
+    saveText () {
+      console.log('save')
+      dialog.showSaveDialog({
+        filters: [{name: 'MarkDown', extensions: ['md']}]},
+      (f) => {
+        console.log(f)
+        fs.writeFileSync(f, this.text)
       })
     }
   }
